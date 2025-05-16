6453fde61421209d52b815e6148861880fd86069
Akshay <pps95026@gmail.com>
2025-05-16 21:53:55 +0530

    added login page

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,10 +4,10 @@ function Header() {
   return (
     <header className="header">
       <div className="logo">BloodAlert</div>
-      <nav>
-        <a href="#">Home</a>
-        <a href="#">Find Donors</a>
-        <a href="#">Login</a>
+      <nav className="hov">
+        <a className="a1" href="#">Home</a>
+        <a className="a2" href="#">Find Donors</a>
+        <a className="a3" href="/login.html">Login</a>
       </nav>
     </header>
   );
